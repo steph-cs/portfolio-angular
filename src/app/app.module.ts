539b5e5b69e7b2db49388951e3f4037807dbb390
steph_cs <stecampelo@gmail.com>
2023-07-25 15:52:52 -0300

    feat: profile section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './template/nav/nav.component';
+import { BannerComponent } from './template/banner/banner.component';
+import { ProfileComponent } from './template/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BannerComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
